445811206abeda781d674c82b0ddde2e2a857b43
Archanagowda23 <archanar249@gmail.com>
2022-04-14 20:53:05 +0530

    Added record match to check records

---
+++ src/test/java/com/bridgelabz/indianstatecensusanalyser/CensusAnalyzerStateCodeTes.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.indianstatecensusanalyser;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CensusAnalyzerStateCodeTes {
+    private static final String INDIA_CENSUS_CSV_PATH = "E:\\eclipseProgram\\untitled\\Census-Analyzer\\src\\main\\resources\\IndiaStateCode.csv";
+
+    @Test
+    public void givenIndianCensusCSVFileReturnsCorrectRecords() {
+        CensusAnalyzer censusAnalyzer = new CensusAnalyzer();
+        try {
+            int numOfRecord = censusAnalyzer.loadIndiaStateCodeData(INDIA_CENSUS_CSV_PATH);
+            Assert.assertEquals(37, numOfRecord);
+        } catch (CensusAnalyzerException e) {
+            e.printStackTrace();
+        }
+    }
+}
